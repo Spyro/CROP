d293fab49ac91aa8d9977c169f14b53db8070791
Diogo Ferreira <diogo@underdev.org>
2009-11-17 01:34:54 +0000

    simplificar mac

--- crop.py
+++ crop.py
@@ -1,11 +1,11 @@
 import random
 
 def mac(pessoas_com_carro, pessoas_sem_carro):
-	p1 = pessoas_com_carro[random.randint(0,len(pessoas_com_carro)-1)]
+	p1 = random.choice(pessoas_com_carro)
 	pessoas_com_carro.remove(p1)
 	pessoas_total = pessoas_com_carro+pessoas_sem_carro
-	p2 = pessoas_total[random.randint(0,len(pessoas_total)-1)]
+	p2 = random.choice(pessoas_total)
 	return (p1,p2)
 
 def porta(pessoas):
-	return pessoas[random.randint(0,len(pessoas)-1)]
+	return random.choice(pessoas)
