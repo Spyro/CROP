327e16a62d393821a3a0ad33c01357b135e0120a
Pedro Correia <spyro@spider.(none)>
2009-11-25 03:38:34 +0000

    Funçao de sorteio de grupos, tamanho exagerado de funcoes adicionado

--- crop.py
+++ crop.py
@@ -1,11 +1,10 @@
 import random
 
 def mac(pessoas_com_carro, pessoas_sem_carro):
-	p1 = random.choice(pessoas_com_carro)
-	pessoas_com_carro.remove(p1)
-	pessoas_total = pessoas_com_carro+pessoas_sem_carro
-	p2 = random.choice(pessoas_total)
-	return (p1,p2)
+	return (pessoas_com_carro.pop(random.randint(0,len(pessoas_com_carro)-1)),random.choice(pessoas_com_carro+pessoas_sem_carro))
 
 def porta(pessoas):
 	return random.choice(pessoas)
+
+def sorteiogrupos(participantes,groups):
+	return [[participantes.pop(random.randint(0,len(participantes)-1)) for k in xrange(0,a)] for a in [len(participantes)/groups + (((len(participantes)%groups) > i and 1) or 0) for i in xrange(0,groups)]
