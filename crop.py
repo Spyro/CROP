a3886f3c6871d6ce264a22bcfaca222c87a27683
Pedro Correia <spyro@spider.(none)>
2009-11-26 19:24:08 +0000

    added cropsort

--- crop.py
+++ crop.py
@@ -8,3 +8,7 @@ def porta(pessoas):
 
 def sorteiogrupos(participantes,groups):
 	return [[participantes.pop(random.randint(0,len(participantes)-1)) for k in xrange(0,a)] for a in [len(participantes)/groups + (((len(participantes)%groups) > i and 1) or 0) for i in xrange(0,groups)]
+
+def cropsort(L):
+	return map(lambda L: L if len(L)==1 else qsort([lt for lt in L[1:] if lt<L[0]]) + [L[0]] + qsort([ge for ge in L[1:] if ge >= L[0]]),[L])[0]
+
